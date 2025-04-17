5f6919e9d8739b85f4a431f201eda8d540808ed2
Sven <svanhuessen@gmail.com>
2025-04-17 22:59:08 +0200

    fix: moved projects

---
+++ solution/prefabs_tests/prefabs_tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace prefabstests
+{
+	TEST_CLASS(prefabstests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
